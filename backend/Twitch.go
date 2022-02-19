aaa3b21b7a6dc9c1300c0a146d955163ffe53966
Ayfri <pierre.ayfri@gmail.com>
2022-02-19 17:24:04 +0100

    refactor: Move Twitch related Structures to TwitchResponses.

--- backend/Twitch.go
+++ backend/Twitch.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
-	"time"
 )
 
 type TwitchClient struct {
@@ -14,80 +13,6 @@ type TwitchClient struct {
 	RedirectURI  string
 	Scopes       []string
 	Token        *TokenResponse
-}
-
-type TokenResponse struct {
-	AccessToken string   `json:"access_token"`
-	ExpiresIn   int      `json:"expires_in"`
-	Scope       []string `json:"scope"`
-	TokenType   string   `json:"token_type"`
-}
-
-func (response *TokenResponse) IsExpired() bool {
-	return time.Now().Unix() > int64(response.ExpiresIn)
-}
-
-func (response *TokenResponse) GetExpiration() time.Time {
-	return time.Now().Add(time.Duration(response.ExpiresIn) * time.Second)
-}
-func (response *TokenResponse) GetFormattedToken() string {
-	t := response.TokenType
-	t = strings.ToUpper(t[:1]) + t[1:]
-	return t + " " + response.AccessToken
-}
-
-type EmoteData struct {
-	Id     string `json:"id"`
-	Name   string `json:"name"`
-	Images struct {
-		Url1X string `json:"url_1x"`
-		Url2X string `json:"url_2x"`
-		Url4X string `json:"url_4x"`
-	} `json:"images"`
-	Tier       string   `json:"tier"`
-	EmoteType  string   `json:"emote_type"`
-	EmoteSetId string   `json:"emote_set_id"`
-	Format     []string `json:"format"`
-	Scale      []string `json:"scale"`
-	ThemeMode  []string `json:"theme_mode"`
-}
-
-type FollowersResponse struct {
-	Total int `json:"total"`
-	Data  []struct {
-		FromId     string    `json:"from_id"`
-		FromLogin  string    `json:"from_login"`
-		FromName   string    `json:"from_name"`
-		ToId       string    `json:"to_id"`
-		ToName     string    `json:"to_name"`
-		FollowedAt time.Time `json:"followed_at"`
-	} `json:"data"`
-	Pagination struct {
-		Cursor string `json:"cursor"`
-	} `json:"pagination"`
-}
-
-type UserData struct {
-	Id              string    `json:"id"`
-	Login           string    `json:"login"`
-	DisplayName     string    `json:"display_name"`
-	Type            string    `json:"type"`
-	BroadcasterType string    `json:"broadcaster_type"`
-	Description     string    `json:"description"`
-	ProfileImageUrl string    `json:"profile_image_url"`
-	OfflineImageUrl string    `json:"offline_image_url"`
-	ViewCount       int       `json:"view_count"`
-	Email           string    `json:"email"`
-	CreatedAt       time.Time `json:"created_at"`
-}
-
-type UserDataResponse struct {
-	Data []UserData `json:"data"`
-}
-
-type EmoteResponse struct {
-	Data     []EmoteData `json:"data"`
-	Template string      `json:"template"`
 }
 
 func (client *TwitchClient) FetchToken() error {
---
+++ backend/TwitchResponses.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"strings"
+	"time"
+)
+
+type EmoteResponse struct {
+	Data     []EmoteData `json:"data"`
+	Template string      `json:"template"`
+}
+
+type EmoteData struct {
+	Id     string `json:"id"`
+	Name   string `json:"name"`
+	Images struct {
+		Url1X string `json:"url_1x"`
+		Url2X string `json:"url_2x"`
+		Url4X string `json:"url_4x"`
+	} `json:"images"`
+	Tier       string   `json:"tier"`
+	EmoteType  string   `json:"emote_type"`
+	EmoteSetId string   `json:"emote_set_id"`
+	Format     []string `json:"format"`
+	Scale      []string `json:"scale"`
+	ThemeMode  []string `json:"theme_mode"`
+}
+
+type FollowersResponse struct {
+	Total int `json:"total"`
+	Data  []struct {
+		FromId     string    `json:"from_id"`
+		FromLogin  string    `json:"from_login"`
+		FromName   string    `json:"from_name"`
+		ToId       string    `json:"to_id"`
+		ToName     string    `json:"to_name"`
+		FollowedAt time.Time `json:"followed_at"`
+	} `json:"data"`
+	Pagination struct {
+		Cursor string `json:"cursor"`
+	} `json:"pagination"`
+}
+
+type TokenResponse struct {
+	AccessToken string   `json:"access_token"`
+	ExpiresIn   int      `json:"expires_in"`
+	Scope       []string `json:"scope"`
+	TokenType   string   `json:"token_type"`
+}
+
+func (response *TokenResponse) GetFormattedToken() string {
+	t := response.TokenType
+	t = strings.ToUpper(t[:1]) + t[1:]
+	return t + " " + response.AccessToken
+}
+
+type UserData struct {
+	Id              string    `json:"id"`
+	Login           string    `json:"login"`
+	DisplayName     string    `json:"display_name"`
+	Type            string    `json:"type"`
+	BroadcasterType string    `json:"broadcaster_type"`
+	Description     string    `json:"description"`
+	ProfileImageUrl string    `json:"profile_image_url"`
+	OfflineImageUrl string    `json:"offline_image_url"`
+	ViewCount       int       `json:"view_count"`
+	Email           string    `json:"email"`
+	CreatedAt       time.Time `json:"created_at"`
+}
+
+type UserDataResponse struct {
+	Data []UserData `json:"data"`
+}
