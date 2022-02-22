2ccddfca2ecfcb7ed6181e5f9287e7e56540cf2a
xhmyjae <tomatis.mt@gmail.com>
2022-02-22 18:40:33 +0100

    feat: work on more and hide button

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,7 +1,49 @@
-const divs = document.querySelectorAll('.streamer-bio');
-for (let i = 0; i < divs.length; i++) {
-    if (divs[i].textContent.length > 60) {
-        divs[i].textContent = divs[i].textContent.substring(0, 60);
-        divs[i].textContent += ' ...'
+cutString = () => {
+    const divs = document.querySelectorAll('.streamer-bio');
+    for (let i = 0; i < divs.length; i++) {
+        if (divs[i].textContent.length > 60) {
+            divs[i].textContent = divs[i].textContent.substring(0, 60);
+            divs[i].textContent += ' ...'
+        }
     }
+}
+
+
+hideStreamers = () => {
+    let dreamsmp = document.querySelectorAll('.dream-smp');
+    let dreamSmp = Array.prototype.slice.call(dreamsmp);
+    let hidden = dreamSmp.slice(-3);
+    hidden.classList.add('dream-hidden');
+}
+
+
+clickStreamers = () => {
+    let dreamsmp = document.querySelectorAll('.dream-smp');
+    let dreamSmp = Array.prototype.slice.call(dreamsmp);
+    let hidden = dreamSmp.slice(-3);
+
+    let more = document.querySelectorAll('.add-more')[0];
+
+    more.onclick = function() {
+        if (more.classList.contains('hidden')) {
+            for (let i = 0; i < hidden.length; i++) {
+                hidden[i].style.display = "flex";
+            }
+            more.textContent = "hide...";
+            more.classList.remove('hidden');
+        } else {
+            for (let i = 0; i < hidden.length; i++) {
+                hidden[i].style.display = "none";
+            }
+            more.textContent = "more..."
+            more.classList.add('hidden');
+        }
+    }
+}
+
+
+window.onload = () => {
+    cutString()
+    hideStreamers()
+    clickStreamers()
 }
